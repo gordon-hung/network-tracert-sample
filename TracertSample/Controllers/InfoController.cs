74d112bedde16f221c2ce10b19087ece51d24d69
Gordon Hung <a932025@gmail.com>
2024-08-17 06:47:46 +0800

    Initial commit

--- TracertSample/Controllers/InfoController.cs
+++ TracertSample/Controllers/InfoController.cs
@@ -9,33 +9,31 @@ namespace TracertSample.Controllers;
 [ApiController]
 public class InfoController : ControllerBase
 {
-    /// <summary>
-    /// Gets the asynchronous.
-    /// </summary>
-    /// <param name="configuration">The configuration.</param>
-    /// <param name="_hostingEnvironment">The hosting environment.</param>
-    /// <returns></returns>
-    [HttpGet]
-    public async Task<object> GetAsync(
-        [FromServices] IConfiguration configuration,
-        [FromServices] IWebHostEnvironment webHostEnvironment)
-    {
-        var hostName = Dns.GetHostName();
-        var hostEntry = await Dns.GetHostEntryAsync(hostName).ConfigureAwait(false);
-        var hostIp = Array.Find(hostEntry.AddressList,
-            x => x.AddressFamily == AddressFamily.InterNetwork);
-        return new
-        {
-            Data = new
-            {
-                Environment.MachineName,
-                HostName = hostName,
-                HostIp = hostIp?.ToString() ?? string.Empty,
-                Environment = webHostEnvironment.EnvironmentName,
-                OsVersion = $"{Environment.OSVersion}",
-                Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString(),
-                ProcessCount = Environment.ProcessorCount
-            }
-        };
-    }
+	/// <summary>
+	/// Gets the asynchronous.
+	/// </summary>
+	/// <param name="webHostEnvironment">The web host environment.</param>
+	/// <returns></returns>
+	[HttpGet]
+	public async Task<object> GetAsync(
+		[FromServices] IWebHostEnvironment webHostEnvironment)
+	{
+		var hostName = Dns.GetHostName();
+		var hostEntry = await Dns.GetHostEntryAsync(hostName).ConfigureAwait(false);
+		var hostIp = Array.Find(hostEntry.AddressList,
+			x => x.AddressFamily == AddressFamily.InterNetwork);
+		return new
+		{
+			Data = new
+			{
+				Environment.MachineName,
+				HostName = hostName,
+				HostIp = hostIp?.ToString() ?? string.Empty,
+				Environment = webHostEnvironment.EnvironmentName,
+				OsVersion = $"{Environment.OSVersion}",
+				Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString(),
+				ProcessCount = Environment.ProcessorCount
+			}
+		};
+	}
 }
--- TracertSample/Controllers/TracertController.cs
+++ TracertSample/Controllers/TracertController.cs
@@ -13,55 +13,63 @@ namespace TracertSample.Controllers;
 [ApiController]
 public class TracertController : ControllerBase
 {
-    [HttpGet("{domain}")]
-    public async IAsyncEnumerable<TracertEntry> GetTracertAsync(
-        [FromServices] Ping ping,
-        string domain = "google.com",
-        [FromQuery] int maxHops = 30)
-    {
-        var ipAddress = (Dns.GetHostAddresses(domain).FirstOrDefault()?.ToString())
-            ?? throw new ArgumentException(string.Format("{0} is not a valid IP address.", domain));
+	/// <summary>
+	/// Gets the tracert asynchronous.
+	/// </summary>
+	/// <param name="ping">The ping.</param>
+	/// <param name="domain">The domain.</param>
+	/// <param name="maxHops">The maximum hops.</param>
+	/// <returns></returns>
+	/// <exception cref="System.ArgumentException"></exception>
+	[HttpGet("{domain}")]
+	public async IAsyncEnumerable<TracertEntry> GetTracertAsync(
+		[FromServices] Ping ping,
+		string domain = "google.com",
+		[FromQuery] int maxHops = 30)
+	{
+		var ipAddress = (Dns.GetHostAddresses(domain).FirstOrDefault()?.ToString())
+			?? throw new ArgumentException(string.Format("{0} is not a valid IP address.", domain));
 
-        // Ensure that the argument address is valid.
-        if (!IPAddress.TryParse(ipAddress, out IPAddress? address))
-            throw new ArgumentException(string.Format("{0} is not a valid IP address.", ipAddress));
+		// Ensure that the argument address is valid.
+		if (!IPAddress.TryParse(ipAddress, out IPAddress? address))
+			throw new ArgumentException(string.Format("{0} is not a valid IP address.", ipAddress));
 
-        var _defaultSendBuffer = new byte[32];
-        for (int i = 0; i < 32; i++)
-            _defaultSendBuffer[i] = (byte)((int)'a' + i % 23);
+		var _defaultSendBuffer = new byte[32];
+		for (int i = 0; i < 32; i++)
+			_defaultSendBuffer[i] = (byte)((int)'a' + i % 23);
 
-        PingOptions pingOptions = new PingOptions(1, true);
-        Stopwatch pingReplyTime = new Stopwatch();
-        PingReply reply;
-        do
-        {
-            pingReplyTime.Start();
-            reply = await ping.SendPingAsync(address, timeout: TimeSpan.FromSeconds(5), buffer: null, options: pingOptions, cancellationToken: HttpContext.RequestAborted).ConfigureAwait(false);
-            pingReplyTime.Stop();
+		PingOptions pingOptions = new PingOptions(1, true);
+		Stopwatch pingReplyTime = new Stopwatch();
+		PingReply reply;
+		do
+		{
+			pingReplyTime.Start();
+			reply = await ping.SendPingAsync(address, timeout: TimeSpan.FromSeconds(5), buffer: null, options: pingOptions, cancellationToken: HttpContext.RequestAborted).ConfigureAwait(false);
+			pingReplyTime.Stop();
 
-            string hostname = string.Empty;
-            if (reply.Address != null && reply.Address.ToString() != IPAddress.Any.ToString() && reply.Address.ToString() != IPAddress.IPv6Any.ToString())
-            {
-                try
-                {
-                    hostname = (await Dns.GetHostEntryAsync(reply.Address).ConfigureAwait(false)).HostName;    // Retrieve the hostname for the replied address.
-                }
-                catch (SocketException) { /* No host available for that address. */ }
-            }
+			string hostname = string.Empty;
+			if (reply.Address != null && reply.Address.ToString() != IPAddress.Any.ToString() && reply.Address.ToString() != IPAddress.IPv6Any.ToString())
+			{
+				try
+				{
+					hostname = (await Dns.GetHostEntryAsync(reply.Address).ConfigureAwait(false)).HostName;    // Retrieve the hostname for the replied address.
+				}
+				catch (SocketException) { /* No host available for that address. */ }
+			}
 
-            // Return out TracertEntry object with all the information about the hop.
-            yield return new TracertEntry()
-            {
-                HopID = pingOptions.Ttl,
-                Address = reply.Address == null ? "N/A" : reply.Address.ToString(),
-                Hostname = hostname,
-                ReplyTime = pingReplyTime.ElapsedMilliseconds,
-                ReplyStatus = reply.Status
-            };
+			// Return out TracertEntry object with all the information about the hop.
+			yield return new TracertEntry()
+			{
+				HopID = pingOptions.Ttl,
+				Address = reply.Address == null ? "N/A" : reply.Address.ToString(),
+				Hostname = hostname,
+				ReplyTime = pingReplyTime.ElapsedMilliseconds,
+				ReplyStatus = reply.Status
+			};
 
-            pingOptions.Ttl++;
-            pingReplyTime.Reset();
-        }
-        while (reply.Status != IPStatus.Success && pingOptions.Ttl <= maxHops);
-    }
+			pingOptions.Ttl++;
+			pingReplyTime.Reset();
+		}
+		while (reply.Status != IPStatus.Success && pingOptions.Ttl <= maxHops);
+	}
 }
