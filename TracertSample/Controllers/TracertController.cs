e99f0a7e1656d1ac86ec628e48d6e0c320937f17
Gordon Hung <a932025@gmail.com>
2024-08-16 22:47:44 +0800

    Initial commit

---
+++ TracertSample/Controllers/TracertController.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TracertSample.Models;
+
+namespace TracertSample.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TracertController : ControllerBase
+{
+    [HttpGet("{domain}")]
+    public async IAsyncEnumerable<TracertEntry> GetTracertAsync(
+        [FromServices] Ping ping,
+        string domain = "google.com",
+        [FromQuery] int maxHops = 30)
+    {
+        var ipAddress = (Dns.GetHostAddresses(domain).FirstOrDefault()?.ToString())
+            ?? throw new ArgumentException(string.Format("{0} is not a valid IP address.", domain));
+
+        // Ensure that the argument address is valid.
+        if (!IPAddress.TryParse(ipAddress, out IPAddress? address))
+            throw new ArgumentException(string.Format("{0} is not a valid IP address.", ipAddress));
+
+        var _defaultSendBuffer = new byte[32];
+        for (int i = 0; i < 32; i++)
+            _defaultSendBuffer[i] = (byte)((int)'a' + i % 23);
+
+        PingOptions pingOptions = new PingOptions(1, true);
+        Stopwatch pingReplyTime = new Stopwatch();
+        PingReply reply;
+        do
+        {
+            pingReplyTime.Start();
+            reply = await ping.SendPingAsync(address, timeout: TimeSpan.FromSeconds(5), buffer: null, options: pingOptions, cancellationToken: HttpContext.RequestAborted).ConfigureAwait(false);
+            pingReplyTime.Stop();
+
+            string hostname = string.Empty;
+            if (reply.Address != null && reply.Address.ToString() != IPAddress.Any.ToString() && reply.Address.ToString() != IPAddress.IPv6Any.ToString())
+            {
+                try
+                {
+                    hostname = (await Dns.GetHostEntryAsync(reply.Address).ConfigureAwait(false)).HostName;    // Retrieve the hostname for the replied address.
+                }
+                catch (SocketException) { /* No host available for that address. */ }
+            }
+
+            // Return out TracertEntry object with all the information about the hop.
+            yield return new TracertEntry()
+            {
+                HopID = pingOptions.Ttl,
+                Address = reply.Address == null ? "N/A" : reply.Address.ToString(),
+                Hostname = hostname,
+                ReplyTime = pingReplyTime.ElapsedMilliseconds,
+                ReplyStatus = reply.Status
+            };
+
+            pingOptions.Ttl++;
+            pingReplyTime.Reset();
+        }
+        while (reply.Status != IPStatus.Success && pingOptions.Ttl <= maxHops);
+    }
+}
---
+++ TracertSample/Program.cs
@@ -0,0 +1,48 @@
+using System.Net.Mime;
+using System.Net.NetworkInformation;
+using System.Text.Json.Serialization;
+
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services
+    .AddRouting(options => options.LowercaseUrls = true)
+    .AddControllers(options =>
+    {
+        options.Filters.Add(new ProducesAttribute(MediaTypeNames.Application.Json));
+        options.Filters.Add(new ConsumesAttribute(MediaTypeNames.Application.Json));
+    })
+    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    foreach (var xmlFileName in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.xml"))
+    {
+        c.IncludeXmlComments(xmlFileName);
+    }
+});
+
+builder.Services.AddTransient<Ping>();
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHealthChecks("/healthz");
+
+app.Run();
